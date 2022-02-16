a4b58d40fc0c25851d04072bbfa4cc2709b8029f
brob <bryanlrobinson@gmail.com>
2022-02-16 14:54:41 -0500

    Adds orbit activity add function

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const OrbitActivities = require('@orbit-love/activities')
+
+
 
 try {
+    
+    const workspace = core.getInput('workspace');
+    const apiKey = core.getInput('api_key');
+    const orbitActivities = new OrbitActivities(workspace, apiKey)
+    const data = {
+        activity_type: `discussion:${github.context.payload.action}`,
+        title: github.context.payload.discussion.title,
+        description: github.context.payload.discussion.body,
+        identity: {
+            type: 'github',
+            id: github.context.payload.discussion.author.login,
+        }
+    }
+    console.log(data)
+    orbitActivities.createActivity(data).then(data => {
+        console.log('success', data)
+    }).catch(error => {
+        console.error(error)
+    })
+    
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
