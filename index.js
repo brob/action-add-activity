9eef9d027d493d9f4a147daed8dbd0b7ba6a14a5
brob <bryanlrobinson@gmail.com>
2022-02-16 16:11:21 -0500

    Switches to using workflow data

--- index.js
+++ index.js
@@ -13,56 +13,22 @@ try {
     const username = core.getInput('username')
     const title = core.getInput('title')
     const occured_at = core.getInput('occured_at')
-    const description = core.getInput('description')
+    const description = core.getMultilineInput('description')
 
     const activity = {
         activity_type: activity_type,
-        username: username,
         title: title,
         occured_at: occured_at,
         description: description,
         member: {
             source: 'github',
-            username: github.context.actor,
+            username: username,
         }
     }
     console.log(activity);
-    
-    
-    
-    let data;
-    if (github.context.payload.comment) {
-        data = {
-            activity_type: `discussion:comment`,
-            link: github.context.payload.comment.html_url,
-            link_text: 'View the discussion',
-            title: `Discussion comment`,
-            occured_at: github.context.payload.comment.created_at,
-            description: `
-${github.context.payload.comment.body}`,
-            member: {
-                github: github.context.payload.comment.user.login,
-            }
-        }
-    } else {
-        data = {
-            activity_type: `discussion:${github.context.payload.action}`,
-            link: github.context.payload.discussion.html_url,
-            link_text: 'View the discussion',
-            title: `Discussion ${github.context.payload.action}`,
-            occured_at: github.context.payload.discussion.created_at,
-            description: `
-# ${github.context.payload.discussion.title}
-${github.context.payload.discussion.body}`,
-            member: {
-                github: github.context.payload.discussion.user.login,
-            }
-        }
-    }
-    
-    
 
-    orbitActivities.createActivity(data).then(data => {
+
+    orbitActivities.createActivity(activity).then(data => {
         core.setOutput("activity", JSON.stringify(data, null, 2))
     }).catch(error => {
         console.error(error)
